be4cabce711927b25d547ab4d64c1d973c063b71
Wilfred Hughes <me@wilfred.me.uk>
2013-07-25 11:59:32 +0100

    adding metadata to keep PyPI happy

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from distutils.core import setup
 
 setup(name='django_function_cache',
+      description="Cache (memoize) a function's output using Django's caching API",
       version='1.0',
+      url="https://github.com/Wilfred/django-function-cache",
+      maintainer="Wilfred Hughes",
+      maintainer_email="me@wilfred.me.uk",
+      license='MIT',
       py_modules=['cached'],
       )
