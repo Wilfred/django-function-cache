ee046bec25ec2e9a8c74d1712290835b3ecb10a1
Wilfred Hughes <me@wilfred.me.uk>
2014-02-11 15:22:33 +0000

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='django_function_cache',
       description="Cache (memoize) a function's output using Django's caching API",
-      version='1.1',
+      version='1.2',
       url="https://github.com/Wilfred/django-function-cache",
       maintainer="Wilfred Hughes",
       maintainer_email="me@wilfred.me.uk",
